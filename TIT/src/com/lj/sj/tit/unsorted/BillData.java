976733c8cbd1571b35fe5d9419e03efe8c290cd3
some.unique.login <some.unique.login@4fa9568a-221e-11df-9960-99068da31be5>
2010-02-26 20:58:14 +0000

    Initial commit

---
+++ TIT/src/com/lj/sj/tit/unsorted/BillData.java
@@ -0,0 +1,15 @@
+package com.lj.sj.tit.unsorted;
+
+import java.math.BigDecimal;
+
+public class BillData {
+	private BigDecimal total;
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+}
---
+++ TIT/src/com/lj/sj/tit/unsorted/BillParser.java
@@ -0,0 +1,10 @@
+package com.lj.sj.tit.unsorted;
+
+import java.awt.Image;
+
+public class BillParser {
+	public BillData parse(Image image)
+	{
+		return null;
+	}
+}
